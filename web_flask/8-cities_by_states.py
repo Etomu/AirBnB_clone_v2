295a2f5e57ac2ae69d7e04c925412bb4656a5b9b
tobi <113621482+Etomu@users.noreply.github.com>
2023-05-21 22:10:05 +0100

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /cities_by_states: HTML page with a list of all states and related cities.
-"""
-from models import storage
-from flask import Flask
-from flask import render_template
+"""a prgram with flask"""
+
+#from models import storage
+#from models.state import State
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
-    States/cities are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
-
-
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
-    storage.close()
+def fun10():
+    """a program with flask"""
+    return storage.close()
 
+
+@app.route('/states_list', strict_slashes=False)
+def fun11():
+    """a program with flask"""
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port="5000")
+#    app.run()
